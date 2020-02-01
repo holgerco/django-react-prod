f028500b98ce1e138f8609f457105a9656316fa5
mohammad hosein shamsaei <mh.shamsaei.ms@gmail.com>
2020-02-01 23:49:21 +0330

    Divide and structure settings

---
+++ src/backend/backend/settings/__init__.py
@@ -0,0 +1,10 @@
+import os
+from .installed_apps import INSTALLED_APPS
+from .middleware import MIDDLEWARE
+from .templates import TEMPLATES
+from .databases import DATABASES
+from .auth import AUTH_USER_MODEL, AUTH_PASSWORD_VALIDATORS, SECRET_KEY
+from .localization import LANGUAGE_CODE, TIME_ZONE, USE_I18N, USE_L10N, USE_TZ
+from .urls import ROOT_URLCONF
+from .static import STATIC_ROOT, MEDIA_ROOT, MEDIA_URL, STATIC_URL, STATICFILES_DIRS
+from .server import ALLOWED_HOSTS, DEBUG, WSGI_APPLICATION
